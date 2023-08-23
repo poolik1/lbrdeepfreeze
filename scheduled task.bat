f22815db554aba3393c2c1178630a9e9576d1058
poolik1 <131620716+poolik1@users.noreply.github.com>
2023-08-23 13:37:07 +0100

    Update scheduled task.bat

--- scheduled task.bat
+++ scheduled task.bat
@@ -1,2 +1,11 @@
 @echo off
-schtasks /create /tn "OpenWebpageTask" /tr "start microsoft-edge.exe https://engage.redbridge.gov.uk/valentines-lido" /sc onlogon /ru %USERNAME%
+
+set "targetURL=https://google.co.uk"
+
+rem Open Edge with the specified URL
+start msedge.exe %targetURL%
+
+rem Create a scheduled task to run at logon
+schtasks /Create /TN "OpenEdgeAtLogon" /SC ONLOGON /TR "msedge.exe %targetURL%" /F
+
+echo Scheduled task created to open Edge at logon.

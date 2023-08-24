f2798f07d6f2e03bd16436eba750eb5a8245bcc0
poolik1 <131620716+poolik1@users.noreply.github.com>
2023-08-24 11:40:30 +0100

    Update set website.bat

--- set website.bat
+++ set website.bat
@@ -1,5 +1,5 @@
 @echo off
-set "URL=https://engage.redbridge.gov.uk/valentines-lido"
+set "URL=URL=https://engage.redbridge.gov.uk/valentines-lido"
 set "ShortcutName=Openwebsite.url"
 set "UserAccount=public_access"
 set "StartupFolder=C:\Users\%UserAccount%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
@@ -7,8 +7,7 @@ set "StartupFolder=C:\Users\%UserAccount%\AppData\Roaming\Microsoft\Windows\Star
 :: Create the shortcut file
 echo [InternetShortcut] > "%StartupFolder%\%ShortcutName%"
 echo URL=%URL% >> "%StartupFolder%\%ShortcutName%"
-echo IconIndex=0 >> "%StartupFolder%\%ShortcutName%"
-echo IconFile=msedge.exe >> "%StartupFolder%\%ShortcutName%"
+echo IconFile=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe >> "%StartupFolder%\%ShortcutName%"
 
-:: Open the website
-start "" "msedge.exe" "%StartupFolder%\%ShortcutName%"
+:: Open the website using Microsoft Edge
+start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "%StartupFolder%\%ShortcutName%"
